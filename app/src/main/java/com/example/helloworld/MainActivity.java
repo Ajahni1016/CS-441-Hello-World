66815b3b79d2de9e4c0b05fb0b9dbc27aa0e8e28
ajahni1016 <alee884@binghamton.edu>
2021-09-03 10:27:44 -0400

    set up button locations

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView helloText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloText = (TextView) findByViewId(R.id.hello_text);
+    }
+}
