7a7402f9930c4877559377637ec176e2faa67f02
ajahni1016 <alee884@binghamton.edu>
2021-09-12 22:05:40 -0400

    color and style fully working

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -71,7 +73,129 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    int ColorFontStyle = 1;
+    int ColorCounter = 1;
+    int FontCounter = 1;
+    int StyleCounter = 1;
+
+    public void changeColor(View view){
+        if(ColorCounter==1){
+            helloText.setTextColor(Color.GRAY);
+            colorText.setText("Gray");
+        }
+        else if(ColorCounter==2){
+            helloText.setTextColor(Color.RED);
+            colorText.setText("Red");
+        }
+        else if(ColorCounter==3){
+            helloText.setTextColor(Color.BLUE);
+            colorText.setText("Blue");
+        }
+        else if(ColorCounter==4){
+            helloText.setTextColor(Color.YELLOW);
+            colorText.setText("Yellow");
+        }
+        else if(ColorCounter==5){
+            helloText.setTextColor(Color.GREEN);
+            colorText.setText("Green");
+        }
+    }
+
+    public void changeFont(View view){
+        if(FontCounter==1){
+//            helloText.setTextColor(Color.GRAY);
+            fontText.setText("Arial");
+        }
+        else if(FontCounter==2){
+//            helloText.setTextColor(Color.RED);
+            fontText.setText("Comic Sans");
+        }
+        else if(FontCounter==3){
+//            helloText.setTypeface(Color.BLUE);
+            fontText.setText(" Times New\n     Roman ");
+        }
+    }
+
+    public void changeStyle(View view){
+        if(StyleCounter==1){
+            helloText.setTypeface(null,Typeface.NORMAL);
+            styleText.setText("Normal");
+        }
+        else if(StyleCounter==2){
+            helloText.setTypeface(null,Typeface.ITALIC);
+            styleText.setText("Italic");
+        }
+        else if(StyleCounter==3){
+            helloText.setTypeface(null,Typeface.BOLD);
+            styleText.setText("Bold");
+        }
+    }
+
+    public void onLeftClick(View view){
+        if(ColorFontStyle==1){
+            if(ColorCounter==1){
+                ColorCounter=5;
+            }
+            else{
+                ColorCounter--;
+            }
+            changeColor(view);
+        }
+
+        else if(ColorFontStyle==2){
+            if(FontCounter==1){
+                FontCounter=3;
+            }
+            else{
+                FontCounter--;
+            }
+            changeFont(view);
+        }
+
+        else if(ColorFontStyle==3){
+            if(StyleCounter==1){
+                StyleCounter=3;
+            }
+            else{
+                StyleCounter--;
+            }
+            changeStyle(view);
+        }
+    }
+
+    public void onRightClick(View view){
+        if(ColorFontStyle==1){
+            if(ColorCounter==5){
+                ColorCounter=1;
+            }
+            else{
+                ColorCounter++;
+            }
+            changeColor(view);
+        }
+        else if(ColorFontStyle==2){
+            if(FontCounter==3){
+                FontCounter=1;
+            }
+            else{
+                FontCounter++;
+            }
+            changeFont(view);
+        }
+
+        else if(ColorFontStyle==3){
+            if(StyleCounter==3){
+                StyleCounter=1;
+            }
+            else{
+                StyleCounter++;
+            }
+            changeStyle(view);
+        }
+    }
+
     public void onColorClick(View view){
+        ColorFontStyle = 1;
         colorButton.setVisibility(View.GONE);
         fontButton.setVisibility(View.GONE);
         styleButton.setVisibility(View.GONE);
@@ -83,6 +207,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onFontClick(View view){
+        ColorFontStyle = 2;
         colorButton.setVisibility(View.GONE);
         fontButton.setVisibility(View.GONE);
         styleButton.setVisibility(View.GONE);
@@ -94,6 +219,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStyleClick(View view){
+        ColorFontStyle = 3;
         colorButton.setVisibility(View.GONE);
         fontButton.setVisibility(View.GONE);
         styleButton.setVisibility(View.GONE);
